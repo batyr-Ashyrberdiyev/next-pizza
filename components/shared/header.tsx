5b2fd5687b1415853ace7871776a1e137aca1747
batyr-Ashyrberdiyev <awbatyr@gmail.com>
2024-08-01 17:41:02 +0500

    commit

---
+++ components/shared/header.tsx
@@ -0,0 +1,48 @@
+import Image from "next/image";
+import { Container } from "./index";
+import { Button } from "../ui";
+import { ArrowRight, ShoppingCart, User } from "lucide-react";
+
+interface Props {
+  className?: string;
+}
+
+export const Header: React.FC<Props> = ({ className }) => {
+  return (
+    <header className={className}>
+      <Container className="flex items-center justify-between py-8">
+        <div className="flex items-center gap-4">
+          <Image src="/logo.png" alt="Logo" width={35} height={35} />
+
+          <div className="">
+            <h1 className="text-2xl uppercase font-black">Next Pizza</h1>
+            <p className="text-sm text-gray-400 leading-3">
+              вкусней уже некуда
+            </p>
+          </div>
+        </div>
+
+        <div className="flex items-center gap-3">
+          <Button variant="outline" className="flex items-center gap-3">
+            <User size={16} />
+            Войти
+          </Button>
+        </div>
+
+        <div className="">
+          <Button className="group relative">
+            <b>502 р.</b>
+
+            <span className="h-full w-[1px] bg-white/30 mx-3" />
+
+            <div className="flex items-center gap-1 transition duration-300 group-hover:opacity-0">
+              <ShoppingCart className="size-4 relative" strokeWidth={2} />
+              <b>3</b>
+            </div>
+            <ArrowRight className="w-5 absolute right-5 transition duration-300 -translate-x-2 opacity-0 group-hover:opacity-100 group-hover:translate-x-0" />
+          </Button>
+        </div>
+      </Container>
+    </header>
+  );
+};
---
+++ components/ui/index.ts
@@ -0,0 +1,5 @@
+export { Button } from "./button";
+export { Checkbox } from "./checkbox";
+export { Popover } from "./popover";
+export { Input } from "./input";
+export { Slider } from "./slider";
