6fe8ef48a4e00610be58118d209dc1099079ab08
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-09-11 17:47:16 +0500

    delete clg

--- components/shared/product-group-list.tsx
+++ components/shared/product-group-list.tsx
@@ -1,11 +1,11 @@
-"use client";
+'use client';
 
-import { FC, useEffect } from "react";
-import { Title } from "./title";
-import { cn } from "@/lib/utils";
-import { ProductCard } from "./product-card";
-import { useIntersectionObserver } from "usehooks-ts";
-import { useZusCategory } from "@/store/category";
+import { FC, useEffect } from 'react';
+import { Title } from './title';
+import { cn } from '@/lib/utils';
+import { ProductCard } from './product-card';
+import { useIntersectionObserver } from 'usehooks-ts';
+import { useZusCategory } from '@/store/category';
 
 interface Props {
   title: string;
@@ -34,13 +34,11 @@ export const ProductGroupList: FC<Props> = ({
     }
   }, [isIntersecting, categoryId]);
 
-  console.log(activeId);
-
   return (
     <div className={className} id={title} ref={ref}>
       <Title text={title} size="lg" className="font-extrabold mb-5" />
 
-      <div className={cn("grid grid-cols-3 gap-[50px]", listClassName)}>
+      <div className={cn('grid grid-cols-3 gap-[50px]', listClassName)}>
         {items.map((item, i) => (
           <ProductCard key={i} {...item} />
         ))}
