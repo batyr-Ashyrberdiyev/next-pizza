28db053d797d292363e2ed8e26c11b07711abf95
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-11-20 17:19:18 +0500

    dynamic page process

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,7 +1,35 @@
-import React from 'react';
+import { Container, ProductImage, Title } from '@/components/shared';
+import { prisma } from '@/prisma/prisma-client';
+import { notFound } from 'next/navigation';
 
-const ProductPage = ({ params: { id } }: { params: { id: string } }) => {
-  return <div>{id}</div>;
+const ProductPage = async ({ params: { id } }: { params: { id: string } }) => {
+  const product = await prisma.product.findFirst({
+    where: {
+      id: Number(id),
+    },
+  });
+
+  if (!product) {
+    return notFound();
+  }
+
+  return (
+    <Container className="flex flex-col my-10">
+      <div className="flex flex-1">
+        <ProductImage src={product.imageUrl} size={40} />
+
+        <div className="w-[490px] bg-[#FCFCFC] p-7">
+          <Title text={product.name} size="md" className="font-extrabold mb-1" />
+
+          <p className="text-gray-400">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus consequuntur modi
+            veritatis eum est in dolorum asperiores, placeat cupiditate cum possimus deserunt magni
+            officia omnis dolor. Cum laboriosam eius aut!
+          </p>
+        </div>
+      </div>
+    </Container>
+  );
 };
 
 export default ProductPage;
