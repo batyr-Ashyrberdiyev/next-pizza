173c846bed021917031bcde22975dc676d630a60
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-11-23 12:48:04 +0500

    commit

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,6 +1,26 @@
-import { Container, ProductImage, Title } from '@/components/shared';
-import { prisma } from '@/prisma/prisma-client';
-import { notFound } from 'next/navigation';
+import {
+  Container,
+  GroupVariants,
+  ProductImage,
+  Title,
+} from "@/components/shared";
+import { prisma } from "@/prisma/prisma-client";
+import { notFound } from "next/navigation";
+
+const items = [
+  {
+    name: "Маленькая",
+    value: "1",
+  },
+  {
+    name: "Средняя",
+    value: "2",
+  },
+  {
+    name: "Большая",
+    value: "3",
+  },
+];
 
 const ProductPage = async ({ params: { id } }: { params: { id: string } }) => {
   const product = await prisma.product.findFirst({
@@ -19,13 +39,20 @@ const ProductPage = async ({ params: { id } }: { params: { id: string } }) => {
         <ProductImage src={product.imageUrl} size={40} />
 
         <div className="w-[490px] bg-[#FCFCFC] p-7">
-          <Title text={product.name} size="md" className="font-extrabold mb-1" />
+          <Title
+            text={product.name}
+            size="md"
+            className="font-extrabold mb-1"
+          />
 
           <p className="text-gray-400">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus consequuntur modi
-            veritatis eum est in dolorum asperiores, placeat cupiditate cum possimus deserunt magni
-            officia omnis dolor. Cum laboriosam eius aut!
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus
+            consequuntur modi veritatis eum est in dolorum asperiores, placeat
+            cupiditate cum possimus deserunt magni officia omnis dolor. Cum
+            laboriosam eius aut!
           </p>
+
+          <GroupVariants items={items} />
         </div>
       </div>
     </Container>
--- components/shared/group-variants.tsx
+++ components/shared/group-variants.tsx
@@ -1,5 +1,7 @@
-import { cn } from '@/lib/utils';
-import { FC } from 'react';
+"use client";
+
+import { cn } from "@/lib/utils";
+import { FC } from "react";
 
 type Variant = {
   name: string;
@@ -10,16 +12,36 @@ type Variant = {
 interface Props {
   items: readonly Variant[];
   defaultValue?: string;
-  onClick: (value: Variant['value']) => void;
-  selectedValue?: Variant['value'];
+  onClick?: (value: Variant["value"]) => void;
+  selectedValue?: Variant["value"];
   className?: string;
 }
 
-export const GroupVariants: FC<Props> = ({ className, items, selectedValue, onClick }) => {
+export const GroupVariants: FC<Props> = ({
+  className,
+  items,
+  selectedValue,
+  onClick,
+}) => {
   return (
-    <div className={cn('flex justify-between bg-[#F3F3F7] rounded-3xl p-1 select-none', className)}>
+    <div
+      className={cn(
+        "flex justify-between bg-[#F3F3F7] rounded-3xl p-1 select-none",
+        className
+      )}
+    >
       {items.map((item) => (
-        <button key={item.name} onClick={() => onClick?.(item.value)}>
+        <button
+          key={item.name}
+          onClick={() => onClick?.(item.value)}
+          className={cn(
+            "flex items-center justify-center h-[30px] px-5 flex-1 rounded-3xl transition-all duration-400 text-sm",
+            {
+              "bg-white shadow": item.value === selectedValue,
+              "text-gray-500 opacity-50 pointer-events-none": item.disabled,
+            }
+          )}
+        >
           {item.name}
         </button>
       ))}
