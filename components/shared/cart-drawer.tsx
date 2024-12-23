b30f20ca0ee8d739ea9a84ce2a53325060f192b0
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-12-23 08:42:33 +0500

    drawer layout ready

---
+++ components/shared/cart-drawer.tsx
@@ -0,0 +1,58 @@
+import { cn } from "@/lib/utils";
+import { FC, PropsWithChildren } from "react";
+import {
+  Sheet,
+  SheetContent,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "../ui/sheet";
+import Link from "next/link";
+import { Button } from "../ui";
+import { ArrowRight } from "lucide-react";
+
+interface Props {
+  className?: string;
+}
+
+export const CartDrawer: FC<PropsWithChildren<Props>> = ({
+  className,
+  children,
+}) => {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>{children}</SheetTrigger>
+
+      <SheetContent className="flex flex-col justify-between pb-0 bg-[#F4F1EE]">
+        <SheetHeader>
+          <SheetTitle>
+            В корзине <span className="font-bold">3 товара</span>
+          </SheetTitle>
+        </SheetHeader>
+
+        <div className="flex-1 -mx-6 mt-5 overflow-auto scrollbar"></div>
+
+        <SheetFooter className="-mx-6 bg-white p-8">
+          <div className="w-full">
+            <div className="flex mb-4">
+              <span className="flex flex-1 text-lg text-neutral-500">
+                Итого
+                <div className="flex-1 border-b border-dashed border-b-neutral-200 relative -top-1 mx-2"></div>
+              </span>
+
+              <span className="font-bold text-lg ">500 ₽</span>
+            </div>
+
+            <Link href="/cart">
+              <Button type="submit" className="w-full h-12 text-base">
+                Оформить заказ
+                <ArrowRight className="w-5 ml-2" />
+              </Button>
+            </Link>
+          </div>
+        </SheetFooter>
+      </SheetContent>
+    </Sheet>
+  );
+};
