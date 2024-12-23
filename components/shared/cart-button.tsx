e42def605e1c5bbe5573c3dfc854bf6b6c9e9f1c
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-12-23 18:51:58 +0500

    deleted user btn

--- components/shared/cart-button.tsx
+++ components/shared/cart-button.tsx
@@ -1,8 +1,8 @@
-import { cn } from "@/lib/utils";
-import { FC } from "react";
-import { Button } from "../ui";
-import { ArrowRight, ShoppingCart, User } from "lucide-react";
-import { CartDrawer } from "./cart-drawer";
+import { cn } from '@/lib/utils';
+import { FC } from 'react';
+import { Button } from '../ui';
+import { ArrowRight, ShoppingCart, User } from 'lucide-react';
+import { CartDrawer } from './cart-drawer';
 
 interface Props {
   className?: string;
@@ -11,23 +11,17 @@ interface Props {
 export const CartButton: FC<Props> = ({ className }) => {
   return (
     <CartDrawer>
-      <div className={cn("flex items-center gap-4", className)}>
-        <Button variant="outline" className="flex items-center gap-3">
-          <User size={16} />
-          Войти
-        </Button>
-        <Button className="group relative">
-          <b>502 р.</b>
+      <Button className="group relative">
+        <b>502 р.</b>
 
-          <span className="h-full w-[1px] bg-white/30 mx-3" />
+        <span className="h-full w-[1px] bg-white/30 mx-3" />
 
-          <div className="flex items-center gap-1 transition duration-300 group-hover:opacity-0">
-            <ShoppingCart className="size-4 relative" strokeWidth={2} />
-            <b>3</b>
-          </div>
-          <ArrowRight className="w-5 absolute right-5 transition duration-300 -translate-x-2 opacity-0 group-hover:opacity-100 group-hover:translate-x-0" />
-        </Button>
-      </div>
+        <div className="flex items-center gap-1 transition duration-300 group-hover:opacity-0">
+          <ShoppingCart className="size-4 relative" strokeWidth={2} />
+          <b>3</b>
+        </div>
+        <ArrowRight className="w-5 absolute right-5 transition duration-300 -translate-x-2 opacity-0 group-hover:opacity-100 group-hover:translate-x-0" />
+      </Button>
     </CartDrawer>
   );
 };
