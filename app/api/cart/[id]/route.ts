fc3092099d370a642fdff762f626cc67f9462c39
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-12-24 15:11:11 +0500

    api for cart

--- app/api/cart/[id]/route.ts
+++ app/api/cart/[id]/route.ts
@@ -1,10 +1,11 @@
 import { prisma } from '@/prisma/prisma-client';
+import { updateCartTotalAmount } from '@/lib/update-cart-total-amount';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function PATCH(req: NextRequest, { params }: { params: { id: string } }) {
   try {
     const id = Number(params.id);
-    const body = (await req.json()) as { quantity: number };
+    const data = (await req.json()) as { quantity: number };
     const token = req.cookies.get('cartToken')?.value;
 
     if (!token) {
@@ -13,9 +14,26 @@ export async function PATCH(req: NextRequest, { params }: { params: { id: string
 
     const cartItem = await prisma.cartItem.findFirst({
       where: {
-        id: Number(id),
+        id,
       },
     });
+
+    if (!cartItem) {
+      return NextResponse.json({ error: 'Cart item not found' });
+    }
+
+    await prisma.cartItem.update({
+      where: {
+        id,
+      },
+      data: {
+        quantity: data.quantity,
+      },
+    });
+
+    const updateUserCart = await updateCartTotalAmount(token);
+
+    return NextResponse.json(updateUserCart);
   } catch (error) {
     console.error('[CART_PATCH] Server error', error);
     return NextResponse.json({ message: 'Не удалось обновить корзину' }, { status: 500 });
