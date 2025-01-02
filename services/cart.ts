91a5ecc5f0fac32b44be033989eeaeb0f2c05045
Kakabay <2kakabayashyrberdyew@gmail.com>
2025-01-02 07:43:11 +0500

    return in one line

--- services/cart.ts
+++ services/cart.ts
@@ -1,18 +1,21 @@
-import { CartDTO } from './dto/cart.dto';
-import { axiosInstance } from './instatnce';
+import { CartDTO } from "./dto/cart.dto";
+import { axiosInstance } from "./instatnce";
 
 export const getCart = async (): Promise<CartDTO> => {
-  const { data } = await axiosInstance.get<CartDTO>('/cart');
-
-  return data;
+  return (await axiosInstance.get<CartDTO>("/cart")).data;
 };
 
-export const updateItemQuantity = async (id: number, quantity: number): Promise<CartDTO> => {
-  const { data } = await axiosInstance.patch<CartDTO>('/cart/' + id, { quantity });
-
-  return data;
+export const updateItemQuantity = async (
+  id: number,
+  quantity: number
+): Promise<CartDTO> => {
+  return (
+    await axiosInstance.patch<CartDTO>("/cart/" + id, {
+      quantity,
+    })
+  ).data;
 };
 
-export const removeCartItem = async (id: number) => {
-  return (await axiosInstance.delete<CartDTO>('/cart/' + id)).data;
+export const removeCartItem = async (id: number): Promise<CartDTO> => {
+  return (await axiosInstance.delete<CartDTO>("/cart/" + id)).data;
 };
