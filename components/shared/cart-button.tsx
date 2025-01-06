947d905d32f764b4bc4e461ba9b84d6f5e117357
Kakabay <2kakabayashyrberdyew@gmail.com>
2025-01-06 09:40:39 +0500

    added values ​​for the number of cart items and cost

--- components/shared/cart-button.tsx
+++ components/shared/cart-button.tsx
@@ -1,24 +1,34 @@
-import { cn } from '@/lib/utils';
-import { FC } from 'react';
-import { Button } from '../ui';
-import { ArrowRight, ShoppingCart, User } from 'lucide-react';
-import { CartDrawer } from './cart-drawer';
+import { cn } from "@/lib/utils";
+import { FC } from "react";
+import { Button } from "../ui";
+import { ArrowRight, ShoppingCart, User } from "lucide-react";
+import { CartDrawer } from "./cart-drawer";
+import { useCartStore } from "@/store/cart";
 
 interface Props {
   className?: string;
 }
 
 export const CartButton: FC<Props> = ({ className }) => {
+  const [totalAmount, loading, items] = useCartStore((state) => [
+    state.totalAmount,
+    state.loading,
+    state.items,
+  ]);
+
   return (
     <CartDrawer>
-      <Button className="group relative">
-        <b>502 р.</b>
+      <Button
+        loading={loading}
+        className={cn("group relative", { "w-[105px]": loading }, className)}
+      >
+        <b>{totalAmount || 0} р.</b>
 
         <span className="h-full w-[1px] bg-white/30 mx-3" />
 
         <div className="flex items-center gap-1 transition duration-300 group-hover:opacity-0">
           <ShoppingCart className="size-4 relative" strokeWidth={2} />
-          <b>3</b>
+          <b>{items.length}</b>
         </div>
         <ArrowRight className="w-5 absolute right-5 transition duration-300 -translate-x-2 opacity-0 group-hover:opacity-100 group-hover:translate-x-0" />
       </Button>
