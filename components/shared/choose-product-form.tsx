e39dc9d04fc93ca3ea60112865b5c06b84cb5906
Kakabay <2kakabayashyrberdyew@gmail.com>
2025-01-06 09:44:35 +0500

    added loading and sending the product to the button

--- components/shared/choose-product-form.tsx
+++ components/shared/choose-product-form.tsx
@@ -1,32 +1,41 @@
-import { FC } from 'react';
-import { ProductImage } from './product-image';
-import { Title } from './title';
-import { Button } from '../ui/button';
-import { cn } from '@/lib/utils';
+import { FC } from "react";
+import { ProductImage } from "./product-image";
+import { Title } from "./title";
+import { Button } from "../ui/button";
+import { cn } from "@/lib/utils";
 
 interface Props {
-  imageUrl: string;
   name: string;
   items?: any[];
-  onClickAdd?: VoidFunction;
+  loading?: boolean;
+  price: number;
+  imageUrl: string;
   className?: string;
+  onSubmit?: VoidFunction;
 }
 
-export const ChooseProductForm: FC<Props> = ({ className, imageUrl, name, items, onClickAdd }) => {
-  const textDetails = '30 см, традицицонное тесто 30';
-  const totalPrice = 350;
-
+export const ChooseProductForm: FC<Props> = ({
+  name,
+  items,
+  price,
+  loading,
+  onSubmit,
+  imageUrl,
+  className,
+}) => {
   return (
-    <div className={cn('flex flex-1', className)}>
+    <div className={cn("flex flex-1", className)}>
       <ProductImage imageUrl={imageUrl} size={30} />
 
       <div className="w-[490px] bg-[#F7F6F5] p-7">
         <Title text={name} size="md" className="font-extrabold mb-1" />
 
-        <p className="text-gray-400">{textDetails}</p>
-
-        <Button className="h-[55px] px-10 text-base rounded-[18px] w-full mt-10">
-          Добавить в корзину за {totalPrice} ₱
+        <Button
+          loading={loading}
+          onClick={() => onSubmit?.()}
+          className="h-[55px] px-10 text-base rounded-[18px] w-full mt-10"
+        >
+          Добавить в корзину за {price} ₱
         </Button>
       </div>
     </div>
