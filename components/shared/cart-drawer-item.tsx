18c48e5c609e208db9f4363a89ef3d92c37df156
Kakabay <2kakabayashyrberdyew@gmail.com>
2025-01-06 09:41:28 +0500

    added functions for adding and removing cart items

--- components/shared/cart-drawer-item.tsx
+++ components/shared/cart-drawer-item.tsx
@@ -1,14 +1,14 @@
-import { cn } from '@/lib/utils';
-import { FC } from 'react';
+import { cn } from "@/lib/utils";
+import { FC } from "react";
 
-import * as CartItem from './cart-item-details';
-import { CartItemProps } from './cart-item-details/cart-item-details.types';
-import { CountButton } from './';
-import { Trash2Icon } from 'lucide-react';
+import * as CartItem from "./cart-item-details";
+import { CartItemProps } from "./cart-item-details/cart-item-details.types";
+import { CountButton } from "./";
+import { Trash2Icon } from "lucide-react";
 
 interface Props extends CartItemProps {
   className?: string;
-  onClickCountButton: (type: 'plus' | 'minus') => void;
+  onClickCountButton: (type: "plus" | "minus") => void;
   onCLickRemove?: VoidFunction;
 }
 
@@ -20,23 +20,29 @@ export const CartDrawerItem: FC<Props> = ({
   details,
   name,
   quantity,
+  onClickCountButton,
+  onCLickRemove,
 }) => {
   return (
-    <div className={cn('flex bg-white p-5 gap-6', className)}>
+    <div className={cn("flex bg-white p-5 gap-6", className)}>
       <CartItem.Image src={imageUrl} />
 
-      <div className="flex-1">
+      <div className="flex-1 ">
         <CartItem.Info details={details} name={name} />
-      </div>
 
-      <hr className="my-3" />
+        <hr className="my-3" />
 
-      <div className="flex items-center justify-between">
-        <CountButton value={quantity} onClick={(type) => console.log(type)} />
+        <div className="flex  items-center justify-between">
+          <CountButton value={quantity} onClick={onClickCountButton} />
 
-        <div className="flex items-center gap-3">
-          <CartItem.Price value={price} />
-          <Trash2Icon className="text-gray-400 cursor-pointer hover:text-gray-600" size={16} />
+          <div className="flex items-center gap-3">
+            <CartItem.Price value={price} />
+            <Trash2Icon
+              onClick={onCLickRemove}
+              className="text-gray-400 cursor-pointer hover:text-gray-600"
+              size={16}
+            />
+          </div>
         </div>
       </div>
     </div>
