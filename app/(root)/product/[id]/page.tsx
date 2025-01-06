8256f1b8463308080d49d127343793b85f9ee0fd
Kakabay <2kakabayashyrberdyew@gmail.com>
2025-01-06 09:39:54 +0500

    extra spaces removed

--- app/(root)/product/[id]/page.tsx
+++ app/(root)/product/[id]/page.tsx
@@ -45,7 +45,7 @@ const ProductPage = async ({ params: { id } }: { params: { id: string } }) => {
             className="font-extrabold mb-1"
           />
 
-          <p className="text-gray-400 mb-4  ">
+          <p className="text-gray-400 mb-4">
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus
             consequuntur modi veritatis eum est in dolorum asperiores, placeat
             cupiditate cum possimus deserunt magni officia omnis dolor. Cum
