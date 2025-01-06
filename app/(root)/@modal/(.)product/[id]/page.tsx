d99c501332353ae064e6cb34b38a11ec9f64934e
Kakabay <2kakabayashyrberdyew@gmail.com>
2025-01-06 09:38:21 +0500

    conveyed the true to the items

--- app/(root)/@modal/(.)product/[id]/page.tsx
+++ app/(root)/@modal/(.)product/[id]/page.tsx
@@ -1,15 +1,19 @@
-import { ChooseProductModal } from '@/components/shared';
-import { prisma } from '@/prisma/prisma-client';
-import { notFound } from 'next/navigation';
+import { ChooseProductModal } from "@/components/shared";
+import { prisma } from "@/prisma/prisma-client";
+import { notFound } from "next/navigation";
 
-export default async function ProductModalPage({ params: { id } }: { params: { id: string } }) {
+export default async function ProductModalPage({
+  params: { id },
+}: {
+  params: { id: string };
+}) {
   const product = await prisma.product.findFirst({
     where: {
       id: Number(id),
     },
     include: {
       ingredients: true,
-      items: {},
+      items: true,
     },
   });
 
