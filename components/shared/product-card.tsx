fb730ff69c6bf6826dcfb0e6896538ee9d55d601
Kakabay <2kakabayashyrberdyew@gmail.com>
2025-01-07 17:29:38 +0500

    prettier

--- components/shared/product-card.tsx
+++ components/shared/product-card.tsx
@@ -1,10 +1,10 @@
-import Link from "next/link";
-import { FC } from "react";
-import { Title } from "./title";
-import { Button } from "../ui";
-import { Plus } from "lucide-react";
-import { cn } from "@/lib/utils";
-import { Ingredient } from "@prisma/client";
+import Link from 'next/link';
+import { FC } from 'react';
+import { Title } from './title';
+import { Button } from '../ui';
+import { Plus } from 'lucide-react';
+import { cn } from '@/lib/utils';
+import { Ingredient } from '@prisma/client';
 
 interface Props {
   name: string;
@@ -15,18 +15,11 @@ interface Props {
   ingredients: Ingredient[];
 }
 
-export const ProductCard: FC<Props> = ({
-  className,
-  name,
-  id,
-  price,
-  imageUrl,
-  ingredients,
-}) => {
+export const ProductCard: FC<Props> = ({ className, name, id, price, imageUrl, ingredients }) => {
   return (
-    <div className={cn(className, "group min-h-[450px]")}>
+    <div className={cn(className, 'group min-h-[450px]')}>
       <div className="flex flex-col justify-between h-full">
-        <Link href={"/product/" + id}>
+        <Link href={'/product/' + id}>
           <div className="flex justify-center p-6 bg-secondary rounded-lg h-[260px]">
             <img
               width={215}
@@ -41,16 +34,16 @@ export const ProductCard: FC<Props> = ({
           <Title text={name} size="sm" className="mb-1 mt-3 font-bold" />
 
           <p className="text-sm text-gray-400 line-clamp-3">
-            {ingredients.map((ingredient) => ingredient.name).join(", ")}
+            {ingredients.map((ingredient) => ingredient.name).join(', ')}
           </p>
         </div>
         <div className="flex justify-between items-center mt-4">
           <span className="text-[20px]">
-            от <b>{price}</b>
+            от <b>{price} ₱</b>
           </span>
 
-          <Link href={"/product/" + id}>
-            <Button variant={"secondary"} className="text-base font-bold">
+          <Link href={'/product/' + id}>
+            <Button variant={'secondary'} className="text-base font-bold">
               <Plus size={20} className="mr-1" />
               Добавить
             </Button>
