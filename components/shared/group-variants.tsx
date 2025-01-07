2e337e2ba1fdbcbf7587c1d7d0f83ba2a2fc4ca8
Kakabay <2kakabayashyrberdyew@gmail.com>
2025-01-07 17:29:29 +0500

    removed unecessary extra spaces

--- components/shared/group-variants.tsx
+++ components/shared/group-variants.tsx
@@ -1,7 +1,7 @@
-"use client";
+'use client';
 
-import { cn } from "@/lib/utils";
-import { FC } from "react";
+import { cn } from '@/lib/utils';
+import { FC } from 'react';
 
 export type Variant = {
   name: string;
@@ -12,36 +12,25 @@ export type Variant = {
 interface Props {
   items: readonly Variant[];
   defaultValue?: string;
-  onClick?: (value: Variant["value"]) => void;
-  value?: Variant["value"];
+  onClick?: (value: Variant['value']) => void;
+  value?: Variant['value'];
   className?: string;
 }
 
-export const GroupVariants: FC<Props> = ({
-  className,
-  items,
-  value,
-  onClick,
-}) => {
+export const GroupVariants: FC<Props> = ({ className, items, value, onClick }) => {
   return (
-    <div
-      className={cn(
-        "flex justify-between bg-[#F3F3F7] rounded-3xl p-1 select-none",
-        className
-      )}
-    >
+    <div className={cn('flex justify-between bg-[#F3F3F7] rounded-3xl p-1 select-none', className)}>
       {items.map((item) => (
         <button
           key={item.name}
           onClick={() => onClick?.(item.value)}
           className={cn(
-            "flex items-center justify-center h-[30px] px-5 flex-1 rounded-3xl transition-all duration-400 text-sm",
+            'flex items-center justify-center h-[30px] px-5 flex-1 rounded-3xl transition-all duration-400 text-sm',
             {
-              "bg-white shadow": item.value === value,
-              "text-gray-500 opacity-50 pointer-events-none": item.disabled,
-            }
-          )}
-        >
+              'bg-white shadow': item.value === value,
+              'text-gray-500 opacity-50 pointer-events-none': item.disabled,
+            },
+          )}>
           {item.name}
         </button>
       ))}
