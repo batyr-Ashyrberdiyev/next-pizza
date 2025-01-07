e66e2d33987fec83b771a2d355f7926e14ec6239
Kakabay <2kakabayashyrberdyew@gmail.com>
2025-01-07 17:30:50 +0500

    prettier

--- lib/get-available-pizza-sizes.ts
+++ lib/get-available-pizza-sizes.ts
@@ -1,18 +1,13 @@
-import { Variant } from "@/components/shared/group-variants";
-import { pizzaSizes, PizzaType } from "@/constantans/pizza";
-import { ProductItem } from "@prisma/client";
+import { ProductItem } from '@prisma/client';
+import { pizzaSizes, PizzaType } from '@/constantans/pizza';
+import { Variant } from '@/components/shared/group-variants';
 
-export const getAvailablePizzaSizes = (
-  type: PizzaType,
-  items: ProductItem[]
-): Variant[] => {
+export const getAvailablePizzaSizes = (type: PizzaType, items: ProductItem[]): Variant[] => {
   const filteredPizzasbyType = items.filter((item) => item.pizzaType === type);
 
   return pizzaSizes.map((item) => ({
     name: item.name,
     value: item.value,
-    disabled: !filteredPizzasbyType.some(
-      (pizza) => Number(pizza.size) === Number(item.value)
-    ),
+    disabled: !filteredPizzasbyType.some((pizza) => Number(pizza.size) === Number(item.value)),
   }));
 };
