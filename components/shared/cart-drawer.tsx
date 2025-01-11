90c8a2fc490d1f7c2f65e5e78fda2e6630a48357
Kakabay <2kakabayashyrberdyew@gmail.com>
2025-01-11 14:47:04 +0500

    added empty cart status

--- components/shared/cart-drawer.tsx
+++ components/shared/cart-drawer.tsx
@@ -1,47 +1,43 @@
-"use client";
+'use client';
 
 import {
   Sheet,
+  SheetClose,
   SheetContent,
   SheetFooter,
   SheetHeader,
   SheetTitle,
   SheetTrigger,
-} from "../ui/sheet";
-import Link from "next/link";
-import { Button } from "../ui";
-import { ArrowRight } from "lucide-react";
-import { getCartItemDetails } from "@/lib";
-import { useCartStore } from "@/store/cart";
-import { CartDrawerItem } from "./cart-drawer-item";
-import { FC, PropsWithChildren, useEffect } from "react";
-import { PizzaSize, PizzaType } from "@/constantans/pizza";
+} from '../ui/sheet';
+import Link from 'next/link';
+import { Button } from '../ui';
+import { ArrowLeft, ArrowRight } from 'lucide-react';
+import { getCartItemDetails } from '@/lib';
+import { useCartStore } from '@/store/cart';
+import { CartDrawerItem } from './cart-drawer-item';
+import { FC, PropsWithChildren, useEffect } from 'react';
+import { PizzaSize, PizzaType } from '@/constantans/pizza';
+import Image from 'next/image';
+import { Title } from './title';
+import { cn } from '@/lib/utils';
 
 export const CartDrawer: FC<PropsWithChildren> = ({ children }) => {
-  const [
-    items,
-    totalAmount,
-    removeCartItem,
-    fetchCartItems,
-    updateItemQuantity,
-  ] = useCartStore((state) => [
-    state.items,
-    state.totalAmount,
-    state.removeCartItem,
-    state.fetchCartItems,
-    state.updateItemQuantity,
-  ]);
+  const [items, totalAmount, removeCartItem, fetchCartItems, updateItemQuantity] = useCartStore(
+    (state) => [
+      state.items,
+      state.totalAmount,
+      state.removeCartItem,
+      state.fetchCartItems,
+      state.updateItemQuantity,
+    ],
+  );
 
   useEffect(() => {
     fetchCartItems();
   }, []);
 
-  const onClickCountButton = (
-    id: number,
-    quantity: number,
-    type: "plus" | "minus"
-  ) => {
-    const newQuantity = type === "plus" ? quantity + 1 : quantity - 1;
+  const onClickCountButton = (id: number, quantity: number, type: 'plus' | 'minus') => {
+    const newQuantity = type === 'plus' ? quantity + 1 : quantity - 1;
     updateItemQuantity(id, newQuantity);
   };
 
@@ -50,61 +46,85 @@ export const CartDrawer: FC<PropsWithChildren> = ({ children }) => {
       <SheetTrigger asChild>{children}</SheetTrigger>
 
       <SheetContent className="flex flex-col justify-between pb-0 bg-[#F4F1EE]">
-        <SheetHeader>
-          <SheetTitle>
-            В корзине <span className="font-bold">{items.length} товара</span>
-          </SheetTitle>
-        </SheetHeader>
+        <div className={cn('flex flex-col h-full', !totalAmount && 'justify-center')}>
+          {totalAmount > 0 && (
+            <SheetHeader>
+              <SheetTitle>
+                В корзине <span className="font-bold">{items.length} товара</span>
+              </SheetTitle>
+            </SheetHeader>
+          )}
 
-        <div className="flex-1 -mx-6 mt-5 overflow-auto">
-          {items.map((item) => {
-            return (
-              <CartDrawerItem
-                disabled
-                className="mb-2"
-                key={item.id}
-                id={item.id}
-                imageUrl={item.imageUrl}
-                details={
-                  item.pizzaSize && item.pizzaType
-                    ? getCartItemDetails(
-                        item.ingredients,
-                        item.pizzaType as PizzaType,
-                        item.pizzaSize as PizzaSize
-                      )
-                    : ""
-                }
-                name={item.name}
-                price={item.price}
-                quantity={item.quantity}
-                onClickCountButton={(type) =>
-                  onClickCountButton(item.id, item.quantity, type)
-                }
-                onCLickRemove={() => removeCartItem(item.id)}
-              />
-            );
-          })}
-        </div>
+          {!totalAmount && (
+            <div className="flex flex-col items-center justify-center w-72 mx-auto">
+              <Image src="/assets/images/empty-box.png" alt="Empty cart" width={120} height={120} />
+              <Title size="sm" text="Корзина пустая" className="text-center font-bold my-2" />
+              <p className="text-center text-neutral-500 mb-5">
+                Добавьте хотя бы одну пицуу, чтобы совершить заказ
+              </p>
 
-        <SheetFooter className="-mx-6 bg-white p-8">
-          <div className="w-full">
-            <div className="flex mb-4">
-              <span className="flex flex-1 text-lg text-neutral-500">
-                Итого
-                <div className="flex-1 border-b border-dashed border-b-neutral-200 relative -top-1 mx-2"></div>
-              </span>
-
-              <span className="font-bold text-lg ">{totalAmount}₱</span>
+              <SheetClose>
+                <Button className="w-56 h-12 text-base" size="lg">
+                  <ArrowLeft className="w-5 mr-2" />
+                  Вернуться назад
+                </Button>
+              </SheetClose>
             </div>
+          )}
 
-            <Link href="/cart">
-              <Button type="submit" className="w-full h-12 text-base">
-                Оформить заказ
-                <ArrowRight className="w-5 ml-2" />
-              </Button>
-            </Link>
-          </div>
-        </SheetFooter>
+          {totalAmount > 0 && (
+            <>
+              <div className="flex-1 -mx-6 mt-5 overflow-auto">
+                {items.map((item) => {
+                  return (
+                    <CartDrawerItem
+                      disabled={item.disabled}
+                      className="mb-2"
+                      key={item.id}
+                      id={item.id}
+                      imageUrl={item.imageUrl}
+                      details={
+                        item.pizzaSize && item.pizzaType
+                          ? getCartItemDetails(
+                              item.ingredients,
+                              item.pizzaType as PizzaType,
+                              item.pizzaSize as PizzaSize,
+                            )
+                          : ''
+                      }
+                      name={item.name}
+                      price={item.price}
+                      quantity={item.quantity}
+                      onClickCountButton={(type) =>
+                        onClickCountButton(item.id, item.quantity, type)
+                      }
+                      onCLickRemove={() => removeCartItem(item.id)}
+                    />
+                  );
+                })}
+              </div>
+              <SheetFooter className="-mx-6 bg-white p-8">
+                <div className="w-full">
+                  <div className="flex mb-4">
+                    <span className="flex flex-1 text-lg text-neutral-500">
+                      Итого
+                      <div className="flex-1 border-b border-dashed border-b-neutral-200 relative -top-1 mx-2"></div>
+                    </span>
+
+                    <span className="font-bold text-lg ">{totalAmount}₱</span>
+                  </div>
+
+                  <Link href="/cart">
+                    <Button type="submit" className="w-full h-12 text-base">
+                      Оформить заказ
+                      <ArrowRight className="w-5 ml-2" />
+                    </Button>
+                  </Link>
+                </div>
+              </SheetFooter>
+            </>
+          )}
+        </div>
       </SheetContent>
     </Sheet>
   );
