6f5a4aace58673b95cb4f2819db9889e310a1513
Kakabay <2kakabayashyrberdyew@gmail.com>
2025-01-13 12:27:33 +0500

    fetched items in checkout list

--- app/(checkout)/checkout/page.tsx
+++ app/(checkout)/checkout/page.tsx
@@ -1,29 +1,128 @@
-import { Container, Title, WhiteBlock } from '@/components/shared';
-import { FC } from 'react';
+"use client";
 
-interface Props {
-  className?: string;
-}
+import {
+  CheckoutItem,
+  CheckoutItemDetails,
+  Container,
+  Title,
+  WhiteBlock,
+} from "@/components/shared";
+import { Button, Input, Textarea } from "@/components/ui";
+import { PizzaSize, PizzaType } from "@/constantans/pizza";
+import { useCart } from "@/hooks";
+import { getCartItemDetails } from "@/lib";
+import { ArrowRight, Package, Percent, Truck } from "lucide-react";
 
 export default function CheckoutPage() {
+  const { items, removeCartItem, onClickCountButton } = useCart();
+
+  console.log(items);
+
   return (
     <Container className="mt-5">
-      <Title text="Оформление заказа" size="xl" className="font-extrabold mb-8" />
+      <Title
+        text="Оформление заказа"
+        size="xl"
+        className="font-extrabold mb-8"
+      />
 
       <div className="flex gap-10">
         <div className="mb-20 flex flex-col gap-10 flex-1">
-          <WhiteBlock title="1. Корзина">123123123123123</WhiteBlock>
+          <WhiteBlock title="1. Корзина">
+            <div className="flex flex-col gap-5">
+              {items.map((item) => (
+                <CheckoutItem
+                  className="mb-2"
+                  key={item.id}
+                  id={item.id}
+                  disabled={item.disabled}
+                  imageUrl={item.imageUrl}
+                  details={getCartItemDetails(
+                    item.ingredients,
+                    item.pizzaType as PizzaType,
+                    item.pizzaSize as PizzaSize
+                  )}
+                  name={item.name}
+                  price={item.price}
+                  quantity={item.quantity}
+                  onClickCountButton={(type) =>
+                    onClickCountButton(item.id, item.quantity, type)
+                  }
+                  onClickRemove={() => removeCartItem(item.id)}
+                />
+              ))}
+            </div>
+          </WhiteBlock>
 
           <WhiteBlock title="2. Персональные данные">
-            <div className="grid grid-cols-4 gap-5"></div>
+            <div className="grid grid-cols-2 gap-5">
+              <Input name="firstname" className="text-base" placeholder="Имя" />
+              <Input
+                name="lastname"
+                className="text-base"
+                placeholder="Фамилия"
+              />
+              <Input name="email" className="text-base" placeholder="E-mail" />
+              <Input name="phone" className="text-base" placeholder="Телефон" />
+            </div>
           </WhiteBlock>
 
           <WhiteBlock title="3. Адрес доставки">
-            <div className="flex flex-col gap-5"></div>
+            <div className="flex flex-col gap-5">
+              <Input name="firstname" className="text-base" placeholder="Имя" />
+              <Textarea
+                className="text-base"
+                rows={5}
+                placeholder="Комментарий к заказу"
+              />
+            </div>
           </WhiteBlock>
         </div>
 
-        <div className="">34324</div>
+        <div className="w-[450px]">
+          <WhiteBlock className="sticky p-6 top-4">
+            <div className="flex flex-col gap-1">
+              <span className="text-xl">Итого:</span>
+              <span className="text-[34px] font-extrabold">3500 ₽</span>
+            </div>
+
+            <CheckoutItemDetails
+              title={
+                <div className="flex items-center">
+                  <Package size={18} className="mr-2 text-gray-300" />
+                  Стоимость товара:
+                </div>
+              }
+              value="3000"
+            />
+            <CheckoutItemDetails
+              title={
+                <div className="flex items-center">
+                  <Percent size={18} className="mr-2 text-gray-300" />
+                  Налоги:
+                </div>
+              }
+              value="3000"
+            />
+            <CheckoutItemDetails
+              title={
+                <div className="flex items-center">
+                  <Truck size={18} className="mr-2 text-gray-300" />
+                  Доставка:
+                </div>
+              }
+              value="3000"
+            />
+
+            <Button
+              type="submit"
+              className="w-full h-14 rounded-xl mt-6 text-base font-bold"
+            >
+              Перейти к оплате
+              <ArrowRight className="w-5 ml-2" />
+            </Button>
+          </WhiteBlock>
+        </div>
       </div>
     </Container>
   );
