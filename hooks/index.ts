bbfba6f9fe325990cf98622771b3fd02b16a80be
Kakabay <2kakabayashyrberdyew@gmail.com>
2025-01-13 12:30:21 +0500

    useCart reexport

--- hooks/index.ts
+++ hooks/index.ts
@@ -1,4 +1,5 @@
+export { useCart } from "./use-cart";
 export { useFilters } from "./use-filters";
-export { useQueryFilters } from "./use-query-filters";
 export { useIngredients } from "./use-ingredients";
 export { usePizzaOptions } from "./use-pizza-options";
+export { useQueryFilters } from "./use-query-filters";
